c2814fede8f114498aadecb88e7615ec87575496
EWIAW <2450925561@qq.com>
2024-04-22 23:16:08 +0800

    1：仅仅反转字母

---
+++ 1：仅仅反转字母/1：仅仅反转字母/test.cpp
@@ -0,0 +1,22 @@
+1.仅仅反转字母
+class Solution {
+public:
+    string reverseOnlyLetters(string s)
+    {
+        int begin = 0;
+        int end = s.size();
+        while (begin < end)
+        {
+            while (begin < end && !isalpha(s[begin]))
+            {
+                begin++;
+            }
+            while (begin < end && !isalpha(s[end]))
+            {
+                end--;
+            }
+            swap(s[begin++], s[end--]);
+        }
+        return s;
+    }
+};
